a69316e309da5f62b1e4b1d622dbb2f6f32d501b
dmitrycx <dkolotsey@gmail.com>
2023-04-06 14:00:38 +0300

    update terraform script, add web api with autoscale and sql server resources

--- main.tf
+++ main.tf
@@ -12,69 +12,69 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "az-204-labs-rg" {
-  name     = "az-204-labs-rg"
+resource "azurerm_resource_group" "az204labs-rg" {
+  name     = "az204labs-rg"
   location = "West Europe"
 }
 
-resource "azurerm_service_plan" "az-204-labs-asp1" {
-  name                = "az-204-labs-asp1"
-  location            = azurerm_resource_group.az-204-labs-rg.location
-  resource_group_name = azurerm_resource_group.az-204-labs-rg.name
+resource "azurerm_service_plan" "az204labs-asp-web-westeu" {
+  name                = "az204labs-asp-web-westeu"
+  location            = azurerm_resource_group.az204labs-rg.location
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
   
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "az-204-labs-app1" {
-  name                = "az-204-labs-app1"
-  location            = azurerm_resource_group.az-204-labs-rg.location
-  resource_group_name = azurerm_resource_group.az-204-labs-rg.name
-  service_plan_id     = azurerm_service_plan.az-204-labs-asp1.id
+resource "azurerm_linux_web_app" "az204labs-app-web-westeu" {
+  name                = "az204labs-app-web-westeu"
+  location            = azurerm_resource_group.az204labs-rg.location
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
+  service_plan_id     = azurerm_service_plan.az204labs-asp-web-westeu.id
 
   site_config {}
 }
 
-resource "azurerm_linux_web_app_slot" "az-204-labs-app1-slot-production" {
-  name                = "az-204-labs-app1-slot-production"
-  app_service_id      = azurerm_linux_web_app.az-204-labs-app1.id
+resource "azurerm_linux_web_app_slot" "web-prod" {
+  name                = "web-prod"
+  app_service_id      = azurerm_linux_web_app.az204labs-app-web-westeu.id
 
   site_config {}
 }
 
-resource "azurerm_linux_web_app_slot" "az-204-labs-app1-slot-development" {
-  name                = "az-204-labs-app1-slot-development"
-  app_service_id      = azurerm_linux_web_app.az-204-labs-app1.id
+resource "azurerm_linux_web_app_slot" "web-dev" {
+  name                = "web-dev"
+  app_service_id      = azurerm_linux_web_app.az204labs-app-web-westeu.id
 
   site_config {}
 }
 
-resource "azurerm_service_plan" "az-204-labs-asp2" {
-  name                = "az-204-labs-asp2"
+resource "azurerm_service_plan" "az204labs-asp-web-eastus" {
+  name                = "az204labs-asp-web-eastus"
   location            = "East US"
-  resource_group_name = azurerm_resource_group.az-204-labs-rg.name
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
   
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "az-204-labs-app2" {
-  name                = "az-204-labs-app2"
-  location            = azurerm_service_plan.az-204-labs-asp2.location
-  resource_group_name = azurerm_resource_group.az-204-labs-rg.name
-  service_plan_id     = azurerm_service_plan.az-204-labs-asp2.id
+resource "azurerm_linux_web_app" "az204labs-app-web-eastus" {
+  name                = "az204labs-app-web-eastus"
+  location            = azurerm_service_plan.az204labs-asp-web-eastus.location
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
+  service_plan_id     = azurerm_service_plan.az204labs-asp-web-eastus.id
 
   site_config {}
 }
 
-resource "azurerm_traffic_manager_profile" "az-204-labs-tm" {
-  name = "az-204-labs-tm"
-  resource_group_name = azurerm_resource_group.az-204-labs-rg.name
+resource "azurerm_traffic_manager_profile" "az204labs-tm" {
+  name = "az204labs-tm"
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
   profile_status = "Enabled"
   traffic_routing_method = "Weighted"
 
   dns_config {
-    relative_name = "az-204-labs-tm"
+    relative_name = "az204labs-tm"
     ttl = 60
   }
 
@@ -85,16 +85,108 @@ resource "azurerm_traffic_manager_profile" "az-204-labs-tm" {
   }
 }
 
-resource "azurerm_traffic_manager_azure_endpoint" "az-204-labs-tm-endpoint1" {
-  name = "az-204-labs-tm-endpoint1"
-  profile_id = azurerm_traffic_manager_profile.az-204-labs-tm.id
-  target_resource_id = azurerm_linux_web_app.az-204-labs-app1.id
+resource "azurerm_traffic_manager_azure_endpoint" "az204labs-tm-endpoint-asp-westeu" {
+  name = "az204labs-tm-endpoint-asp-westeu"
+  profile_id = azurerm_traffic_manager_profile.az204labs-tm.id
+  target_resource_id = azurerm_linux_web_app.az204labs-app-web-westeu.id
   weight = 90
 }
 
-resource "azurerm_traffic_manager_azure_endpoint" "az-204-labs-tm-endpoint2" {
-  name = "az-204-labs-tm-endpoint2"
-  profile_id = azurerm_traffic_manager_profile.az-204-labs-tm.id
-  target_resource_id = azurerm_linux_web_app.az-204-labs-app2.id
+resource "azurerm_traffic_manager_azure_endpoint" "az204labs-tm-endpoint-asp-eastus" {
+  name = "az204labs-tm-endpoint-asp-eastus"
+  profile_id = azurerm_traffic_manager_profile.az204labs-tm.id
+  target_resource_id = azurerm_linux_web_app.az204labs-app-web-eastus.id
   weight = 10
 }
+
+
+resource "azurerm_service_plan" "az204labs-asp-api-westeu" {
+  name                = "az204labs-asp-api-westeu"
+  location            = azurerm_resource_group.az204labs-rg.location
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
+
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "az204labs-app-api-westeu" {
+  name                = "az204labs-app-api-westeu"
+  location            = azurerm_service_plan.az204labs-asp-api-westeu.location
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
+  service_plan_id     = azurerm_service_plan.az204labs-asp-api-westeu.id
+
+  site_config {}
+}
+
+resource "azurerm_monitor_autoscale_setting" "az204labs-app-api-westeu-autoscale" {
+  name                = "az204labs-app-api-westeu-autoscale"
+  resource_group_name = azurerm_resource_group.az204labs-rg.name
+  location            = azurerm_resource_group.az204labs-rg.location
+  target_resource_id  = azurerm_service_plan.az204labs-asp-api-westeu.id
+  
+  
+  profile {
+    name = "autoscale"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 3
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.az204labs-asp-api-westeu.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 75
+        dimensions {
+          name     = "AppName"
+          operator = "Equals"
+          values   = ["App1"]
+        }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.az204labs-asp-api-westeu.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+}
+
+resource "azurerm_mssql_server" "az204labs-sql" {
+  name                         = "az204labs-sql"
+  resource_group_name          = azurerm_resource_group.az204labs-rg.name
+  location                     = azurerm_resource_group.az204labs-rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssw0rd1234"
+}
+
